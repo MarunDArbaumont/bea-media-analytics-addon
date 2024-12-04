2c5851b07d857c43026dd0cbb7835acfca7a0d1e
Marin <marin.maulbon@perfectogrupe.fr>
2024-12-04 17:15:20 +0100

    je sais plus ce que je push

--- bea-media-analytics-addon.php
+++ bea-media-analytics-addon.php
@@ -48,6 +48,34 @@ function add_media_indexing_button(): void {
     );
 }
 
+
+function get_id_from_url( $attachment_url ) {
+	global $wpdb;
+
+	// If there is no url, return.
+	if ( '' == $attachment_url ) {
+		error_log('$attachment_url is empty');
+		return 0;
+	}
+
+	// Get the upload directory paths
+	$upload_dir_paths = wp_upload_dir();
+
+	// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
+	if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
+		
+		// If this is the URL of an auto-generated thumbnail, get the URL of the original image
+		$attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
+
+		// Remove the upload path base directory from the attachment URL
+		$attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
+		error_log('$attachment_url');
+		// Finally, run a custom database query to get the attachment ID from the modified attachment URL
+		return (int) $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = %s AND wposts.post_type = 'attachment'", $attachment_url ) );
+	}
+	return 0;
+}
+
 // Found the media in url:https://...
 function get_media_from_url($media_ids) {
 
@@ -67,32 +95,6 @@ function get_media_from_url($media_ids) {
 	// Loop on medias to get ID instead URL
 	$medias = array_map( 'get_id_from_url', $images );
 	return array_filter( $medias );
-}
-
-function get_id_from_url( $attachment_url ) {
-	global $wpdb;
-
-	// If there is no url, return.
-	if ( '' == $attachment_url ) {
-		error_log('$attachment_url is empty');
-		return 0;
-	}
-
-	// Get the upload directory paths
-	$upload_dir_paths = wp_upload_dir();
-
-	// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
-	if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
-		// If this is the URL of an auto-generated thumbnail, get the URL of the original image
-		$attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
-
-		// Remove the upload path base directory from the attachment URL
-		$attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
-
-		// Finally, run a custom database query to get the attachment ID from the modified attachment URL
-		return (int) $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = %s AND wposts.post_type = 'attachment'", $attachment_url ) );
-	}
-	return 0;
 }
 
 function get_media_from_urls( $media_ids, $post_content ) {
